9f529c6c721861add2d3242a456874ff6499d09a
jakobrasmussen <jakobrasmussen@localhost>
2019-11-20 13:52:52 +0100

    Update notes: - Building: ID + Constructor

--- src/com/company/Building.java
+++ src/com/company/Building.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 
 public class Building {
     private String name;
+    private int id;
     private ArrayList sensors;
     private ArrayList actuators;
 
+    
+    public Building(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+    
     public String getName() {
         return name;
     }
 
+    public int getId()
+    {
+        return id;
+    }
+    
     public ArrayList getSensors() {
         return sensors;
     }
